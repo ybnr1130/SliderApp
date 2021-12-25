b112e1db2b88e9609c89c6af3ab42a5aa027754d
ybnr1130 <muuckk@gmail.com>
2021-12-25 09:20:24 +0900

    First commit

---
+++ SliderApp/SliderApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderApp
+//
+//  Created by 薮内紀仁 on 2021/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SliderApp/SliderApp/SliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppApp.swift
+//  SliderApp
+//
+//  Created by 薮内紀仁 on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

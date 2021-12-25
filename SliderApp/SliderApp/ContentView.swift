1c358cc1b29905edea91f4807108e0b5b69a3d41
ybnr1130 <muuckk@gmail.com>
2021-12-25 09:40:28 +0900

    Add and decorate slider

--- SliderApp/SliderApp/ContentView.swift
+++ SliderApp/SliderApp/ContentView.swift
@@ -5,12 +5,19 @@
 //  Created by 薮内紀仁 on 2021/12/25.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Image(systemName:"speaker.fill")
+            Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+            Image(systemName:"speaker.3.fill")
+        }
+        .accentColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
